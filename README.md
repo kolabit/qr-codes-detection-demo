28dac493b627be841643bd6836182a8937d9692c
kolabit <nikolaybi@gmail.com>
2023-01-29 13:35:01 -0500

    cosmetic changes

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # Streamlit Demo: QR codes detection demo
 
 This project demonstrates the QR codes detection model, based on [Ultralytics YOLOv5](https://github.com/ultralytics/yolov5), trained on custom [QR-Codes dataset](https://github.com/kolabit/qr-codes), and implemented as an interactive [Streamlit](https://streamlit.io) app.
+Demo is based on [Udacity self-driving-car demo](https://share.streamlit.io/streamlit/demo-self-driving)
 
-
+# Run demo:
+Open the [Streamlit QR codes detection demo page] (https://kolabit-qr-codes-detection-demo-streamlit-app-wuj4pl.streamlit.app)
 
--- streamlit_app.py
+++ streamlit_app.py
@@ -13,8 +13,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# This demo lets you to explore the Udacity self-driving car image dataset.
-# More info: https://github.com/streamlit/demo-self-driving
+# This demo lets you to explore and analyze the images of 
+# QR-codes test dataset https://github.com/kolabit/qr-codes
 
 import streamlit as st
 import altair as alt
@@ -26,28 +26,13 @@ import torch
 # Streamlit encourages well-structured code, like starting execution in a main() function.
 def main():
     
-    
-    # Render the readme as markdown using st.markdown.
-    #readme_text = st.markdown(get_file_content_as_string("instructions.md"))
-
     # Download external dependencies.
     for filename in EXTERNAL_DEPENDENCIES.keys():
         download_file(filename)
-    
+    # run the app
     run_the_app()
 
-    # Once we have the dependencies, add a selector for the app mode on the sidebar.
-    #st.sidebar.title("What to do")
-    #app_mode = st.sidebar.selectbox("Choose the app mode",
-    #    ["Show instructions", "Run the app", "Show the source code"])
-    #if app_mode == "Show instructions":
-    #    st.sidebar.success('To continue select "Run the app".')
-    #elif app_mode == "Show the source code":
-    #    readme_text.empty()
-    #    st.code(get_file_content_as_string("streamlit_app.py"))
-    #elif app_mode == "Run the app":
-    #    readme_text.empty()
-    #    run_the_app()
+  
 
 # This file downloader demonstrates Streamlit animation.
 def download_file(file_path):
@@ -113,9 +98,6 @@ def run_the_app():
     # recomputes only whatever subset is required to get the right answer!
     metadata = load_metadata(os.path.join(DATA_URL_ROOT, "labels.csv.gz"))
     summary = create_summary(metadata)
-
-    # Uncomment these lines to peek at these DataFrames.
-    # st.write('## Metadata', metadata[:1000], '## Summary', summary[:1000])
 
     # Draw the UI elements to search for objects (pedestrians, cars, etc.)
     selected_frame_index, selected_frame = frame_selector_ui(summary)
@@ -200,13 +182,6 @@ def draw_image_with_boxes(image, boxes, header, description):
     st.markdown(description)
     st.image(image_with_boxes.astype(np.uint8), use_column_width=True)
 
-# Download a single file and make its content available as a string.
-@st.experimental_singleton(show_spinner=False)
-def get_file_content_as_string(path):
-    url = 'https://raw.githubusercontent.com/streamlit/demo-self-driving/master/' + path
-    response = urllib.request.urlopen(url)
-    return response.read().decode("utf-8")
-
 # This function loads an image from Streamlit public repo on S3. We use st.cache on this
 # function as well, so we can reuse the images across runs.
 @st.experimental_memo(show_spinner=False)
@@ -220,15 +195,19 @@ def load_image(url):
 # Run the YOLO v5 model to detect objects.
 def yolo_v5(image, confidence_threshold ):
 
-    # Model
+    # Create the Model from the PyTorch hub
     model_qr = torch.hub.load('ultralytics/yolov5', 'custom', path='./best.pt') 
-    detections_qr = model_qr(image)
-    xmin, xmax, ymin, ymax, labels = [], [], [], [], []
 
+    # Run the model on the selected image
+    detections_qr = model_qr(image)
+
+    # Build the result lists
+    xmin, xmax, ymin, ymax, labels = [], [], [], [], []
     results = detections_qr.pandas().xyxy[0].to_dict(orient="records")
     for result in results:
         conf_i = result['confidence']
-        if(conf_i >=confidence_threshold):
+        # Filter the boxes by confidence threshold
+        if(conf_i >= confidence_threshold):
             xmin.append(int(result['xmin']))
             ymin.append(int(result['ymin']))
             xmax.append(int(result['xmax']))
@@ -238,152 +217,11 @@ def yolo_v5(image, confidence_threshold ):
     boxes = pd.DataFrame({"xmin": xmin, "ymin": ymin, "xmax": xmax, "ymax": ymax, "labels": labels})
     return boxes[["xmin", "ymin", "xmax", "ymax", "labels"]]
  
-    # Load the network. Because this is cached it will only happen once.
-    #@st.cache(allow_output_mutation=True)
-    #def load_network(config_path, weights_path):
-    #     net = cv2.dnn.readNetFromDarknet(config_path, weights_path)
-    #     output_layer_names = net.getLayerNames()
-    #     output_layer_names = [output_layer_names[i - 1] for i in net.getUnconnectedOutLayers()]
-    #     return net, output_layer_names
-
-    # net, output_layer_names = load_network("yolov3.cfg", "yolov3.weights")
-
-    # # Run the YOLO neural net.
-    # blob = cv2.dnn.blobFromImage(image, 1 / 255.0, (416, 416), swapRB=True, crop=False)
-    # net.setInput(blob)
-    # layer_outputs = net.forward(output_layer_names)
-
-    # # Supress detections in case of too low confidence or too much overlap.
-    # boxes, confidences, class_IDs = [], [], []
-    # H, W = image.shape[:2]
-    # for output in layer_outputs:
-    #     for detection in output:
-    #         scores = detection[5:]
-    #         classID = np.argmax(scores)
-    #         confidence = scores[classID]
-    #         if confidence > confidence_threshold:
-    #             box = detection[0:4] * np.array([W, H, W, H])
-    #             centerX, centerY, width, height = box.astype("int")
-    #             x, y = int(centerX - (width / 2)), int(centerY - (height / 2))
-    #             boxes.append([x, y, int(width), int(height)])
-    #             confidences.append(float(confidence))
-    #             class_IDs.append(classID)
-    # indices = cv2.dnn.NMSBoxes(boxes, confidences, confidence_threshold, overlap_threshold)
-
-    # Map from YOLO labels to Udacity labels.
-    #UDACITY_LABELS = {
-    #    0: 'pedestrian',
-    #    1: 'biker',
-    #    2: 'car',
-    #    3: 'biker',
-    #    5: 'truck',
-    #    7: 'truck',
-    #    9: 'trafficLight'
-    #}
-
-    ####UDACITY_LABELS = {
-    ####    0: 'qr-code',
-    ####}
-    ####xmin, xmax, ymin, ymax, labels = [100,200], [300,400], [120,250], [320,450], ['qr-code', 'qr-code']
-
-    #if len(indices) > 0:
-        # loop over the indexes we are keeping
-    #    for i in indices.flatten():
-    #        label = UDACITY_LABELS.get(class_IDs[i], None)
-    #        if label is None:
-    #            continue
-
-            # extract the bounding box coordinates
-    #        x, y, w, h = boxes[i][0], boxes[i][1], boxes[i][2], boxes[i][3]
-
-    #        xmin.append(x)
-    #        ymin.append(y)
-    #        xmax.append(x+w)
-    #        ymax.append(y+h)
-    #        labels.append(label)
-
-    ### boxes = pd.DataFrame({"xmin": xmin, "ymin": ymin, "xmax": xmax, "ymax": ymax, "labels": labels})
-    ### return boxes[["xmin", "ymin", "xmax", "ymax", "labels"]]
-
-# Run the YOLO model to detect objects.
-def yolo_v3(image, confidence_threshold, overlap_threshold):
-    # Load the network. Because this is cached it will only happen once.
-    @st.cache(allow_output_mutation=True)
-    def load_network(config_path, weights_path):
-        net = cv2.dnn.readNetFromDarknet(config_path, weights_path)
-        output_layer_names = net.getLayerNames()
-        output_layer_names = [output_layer_names[i - 1] for i in net.getUnconnectedOutLayers()]
-        return net, output_layer_names
-    net, output_layer_names = load_network("yolov3.cfg", "yolov3.weights")
-
-    # Run the YOLO neural net.
-    blob = cv2.dnn.blobFromImage(image, 1 / 255.0, (416, 416), swapRB=True, crop=False)
-    net.setInput(blob)
-    layer_outputs = net.forward(output_layer_names)
-
-    # Supress detections in case of too low confidence or too much overlap.
-    boxes, confidences, class_IDs = [], [], []
-    H, W = image.shape[:2]
-    for output in layer_outputs:
-        for detection in output:
-            scores = detection[5:]
-            classID = np.argmax(scores)
-            confidence = scores[classID]
-            if confidence > confidence_threshold:
-                box = detection[0:4] * np.array([W, H, W, H])
-                centerX, centerY, width, height = box.astype("int")
-                x, y = int(centerX - (width / 2)), int(centerY - (height / 2))
-                boxes.append([x, y, int(width), int(height)])
-                confidences.append(float(confidence))
-                class_IDs.append(classID)
-    indices = cv2.dnn.NMSBoxes(boxes, confidences, confidence_threshold, overlap_threshold)
-
-    # Map from YOLO labels to Udacity labels.
-    UDACITY_LABELS = {
-        0: 'pedestrian',
-        1: 'biker',
-        2: 'car',
-        3: 'biker',
-        5: 'truck',
-        7: 'truck',
-        9: 'trafficLight'
-    }
-    xmin, xmax, ymin, ymax, labels = [], [], [], [], []
-    if len(indices) > 0:
-        # loop over the indexes we are keeping
-        for i in indices.flatten():
-            label = UDACITY_LABELS.get(class_IDs[i], None)
-            if label is None:
-                continue
-
-            # extract the bounding box coordinates
-            x, y, w, h = boxes[i][0], boxes[i][1], boxes[i][2], boxes[i][3]
-
-            xmin.append(x)
-            ymin.append(y)
-            xmax.append(x+w)
-            ymax.append(y+h)
-            labels.append(label)
-
-    boxes = pd.DataFrame({"xmin": xmin, "ymin": ymin, "xmax": xmax, "ymax": ymax, "labels": labels})
-    return boxes[["xmin", "ymin", "xmax", "ymax", "labels"]]
 
 # Path to the Streamlit public S3 bucket
-#DATA_URL_ROOT = "https://streamlit-self-driving.s3-us-west-2.amazonaws.com/"
 DATA_URL_ROOT = "https://raw.githubusercontent.com/kolabit/qr-codes/main/images/test/"
 
-# External files to download.
-#EXTERNAL_DEPENDENCIES = {
-#    "yolov3.weights": {
-#        "url": "https://pjreddie.com/media/files/yolov3.weights",
-#        "size": 248007048
-#    },
-#    "yolov3.cfg": {
-#        "url": "https://raw.githubusercontent.com/pjreddie/darknet/master/cfg/yolov3.cfg",
-#        "size": 8342
-#    }
-#}
-
+# Weigths of the YOLOv5 model, trained on QR codes dataset
 EXTERNAL_DEPENDENCIES = {
     "best.pt": {
         "url": "https://github.com/kolabit/qr-codes/raw/main/best.pt",
